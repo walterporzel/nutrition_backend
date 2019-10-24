63a18ff88a2ac63fe2453d32fd465a94afdf2cd9
Walter_Porzel <walterp@vt.edu>
2019-10-24 13:38:21 -0400

    modified settings

--- nutrition_django/settings.py
+++ nutrition_django/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = 'nutrition_django.wsgi.application'
 
 DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
